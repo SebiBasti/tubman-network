b34882942f73fc9a46223824a1ece7c1cb647ba5
Sebastian Remm <sebastian.remm@web.de>
2022-05-08 13:14:20 +0200

    "safari fix"

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,12 @@ export default function Document() {
         {/* --- MATERIAL UI --- */}
         {/* https://mui.com/material-ui/getting-started/installation/ */}
         <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
+        />
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
