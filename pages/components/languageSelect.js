fb045f2bc7eb5aeea04a67231b65294b6ffaef57
Sebastian Remm <sebastian.remm@web.de>
2022-05-08 13:39:10 +0200

    "safari fix"

--- pages/components/languageSelect.js
+++ pages/components/languageSelect.js
@@ -34,7 +34,7 @@ const LanguageSelect = () => {
   return (
     <div className="d-flex justify-content-end align-items-center language-select-root">
       <Button onClick={({ currentTarget }) => setMenuAnchor(currentTarget)}>
-        {languageMap[selected].label || "none"}
+        {/*{languageMap[selected].label || "none"}*/}
         <ArrowDropDownIcon fontSize="small" />
       </Button>
       <Popover
